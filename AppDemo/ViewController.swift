c88173202d365af6d6413f54cf83b56be0c4072c
Development <dev@danieles-macbook.local>
2018-07-30 12:42:29 -0700

    Initial Commit

---
+++ AppDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppDemo
+//
+//  Created by Development on 7/30/18.
+//  Copyright © 2018 Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
